2159be5c2a3483c285afdb9b3f16f77c66ac57e0
DavidWallJ <djkindread@gmail.com>
2017-02-14 20:18:17 +0800

    add /todos and /todos/:id

--- server.js
+++ server.js
@@ -6,8 +6,49 @@ var app = express();
 
 var PORT = process.env.PORT || 3000;
 
+var todos = [{
+    id: 1,
+    desc: "Eat a banana.",
+    completed: false
+},{
+    id: 2,
+    desc: "Buy twist ties.",
+    completed: false
+
+},{
+    id: 3,
+    desc: "Walk down stairs.",
+    completed: true
+
+}];
 app.get('/', function (req, res) {
     res.send('Todo API Root');
+});
+
+app.get('/todos', function (req, res) {
+    res.json(todos);
+});
+
+app.get('/todos/:id', function (req, res) {
+    var todoId = req.params.id;
+
+    // var todoId = parseInt(req.params.id, 10);
+    // since it we want the id as a number instead of a string we need to parseInt to base of 10 OR you can just make it a double == isntead of a triple.  I'm doing this.  It's probably a bad practice for some reason.
+    var matchedTodo;
+
+    // iterate over todos array. find match.
+    todos.forEach(function (todo) {
+        if (todoId == todo.id) {
+            matchedTodo = todo;
+        }
+    });
+
+    if (matchedTodo) {
+        res.json(matchedTodo);
+    } else {
+        res.status(404).send(`Id of '${todoId}' doesn't exist :/`);
+    }
+
 });
 
 app.listen(PORT, function () {
