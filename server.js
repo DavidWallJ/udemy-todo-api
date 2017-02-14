dc222421feea50c8825bb4096672c104017eb961
DavidWallJ <djkindread@gmail.com>
2017-02-14 19:11:33 +0800

    Ready for heroku

---
+++ server.js
@@ -0,0 +1,15 @@
+/**
+ * Created by david on 2/14/17.
+ */
+var express = require('express');
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+    res.send('Todo API Root');
+});
+
+app.listen(PORT, function () {
+   console.log(`Express is listening on ${PORT}!`);
+});
