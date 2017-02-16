e61c9db1cb6c17d429300851da53879ee7471386
DavidWallJ <djkindread@gmail.com>
2017-02-16 20:49:26 +0800

    added DELETE to app

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
  */
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -22,18 +23,8 @@ app.get('/todos', function (req, res) {
 
 // GET /todos/:id
 app.get('/todos/:id', function (req, res) {
-    var todoId = req.params.id;
-
-    // var todoId = parseInt(req.params.id, 10);
-    // since it we want the id as a number instead of a string we need to parseInt to base of 10 OR you can just make it a double == instead of a triple.  I'm doing this.  It's probably a bad practice for some reason.
-    var matchedTodo;
-
-    // iterate over todos array. find match.
-    todos.forEach(function (todo) {
-        if (todoId == todo.id) {
-            matchedTodo = todo;
-        }
-    });
+    var todoId = parseInt(req.params.id);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
 
     if (matchedTodo) {
         res.json(matchedTodo);
@@ -46,6 +37,13 @@ app.get('/todos/:id', function (req, res) {
 // POST /todos
 app.post('/todos', function (req, res) {
     var body = req.body;
+    body = _.pick(body, 'description', 'completed');
+
+    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+        return res.status(400).send();
+    }
+
+    body.description = body.description.trim();
 
     body.id = todoNextId++;
 
@@ -54,6 +52,18 @@ app.post('/todos', function (req, res) {
     res.json(body);
 });
 
+//DELETE /todos/:id
+app.delete('/todos/:id', function (req, res) {
+    var todoId = parseInt(req.params.id);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+
+    if(!matchedTodo) {
+        res.status(404).json({"error": "No todo found"});
+    } else {
+        todos = _.without(todos, matchedTodo);
+        res.send(`Deleted: Item # ${matchedTodo.id}.`);
+    }
+});
 
 app.listen(PORT, function () {
    console.log(`Express is listening on ${PORT}!`);
