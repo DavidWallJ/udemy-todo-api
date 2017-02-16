5a85314e10393d89291d49960dd020e5bcdfd110
DavidWallJ <djkindread@gmail.com>
2017-02-16 19:16:33 +0800

    added POST for todos

--- server.js
+++ server.js
@@ -2,43 +2,30 @@
  * Created by david on 2/14/17.
  */
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
-
 var PORT = process.env.PORT || 3000;
+var todos = [];
+var todoNextId = 1;
 
-var todos = [{
-    id: 1,
-    desc: "Eat a banana.",
-    completed: false
-},{
-    id: 2,
-    desc: "Buy twist ties.",
-    completed: false
+app.use(bodyParser.json());
 
-},{
-    id: 3,
-    desc: "Walk down stairs.",
-    completed: true
-
-},{
-    id: 4,
-    desc: "Take a break.",
-    completed: false
-
-}];
 app.get('/', function (req, res) {
     res.send('Todo API Root');
 });
 
+// GET /todos
 app.get('/todos', function (req, res) {
     res.json(todos);
 });
 
+// GET /todos/:id
 app.get('/todos/:id', function (req, res) {
     var todoId = req.params.id;
 
     // var todoId = parseInt(req.params.id, 10);
-    // since it we want the id as a number instead of a string we need to parseInt to base of 10 OR you can just make it a double == isntead of a triple.  I'm doing this.  It's probably a bad practice for some reason.
+    // since it we want the id as a number instead of a string we need to parseInt to base of 10 OR you can just make it a double == instead of a triple.  I'm doing this.  It's probably a bad practice for some reason.
     var matchedTodo;
 
     // iterate over todos array. find match.
@@ -55,6 +42,18 @@ app.get('/todos/:id', function (req, res) {
     }
 
 });
+
+// POST /todos
+app.post('/todos', function (req, res) {
+    var body = req.body;
+
+    body.id = todoNextId++;
+
+    todos.push(body);
+
+    res.json(body);
+});
+
 
 app.listen(PORT, function () {
    console.log(`Express is listening on ${PORT}!`);
