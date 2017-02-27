530d9610a6699bbca98a539adab06cd1c3d46634
DavidWallJ <djkindread@gmail.com>
2017-02-27 14:31:39 +0800

    force on

--- server.js
+++ server.js
@@ -167,7 +167,7 @@ app.post('/users/login', function (req, res) {
 });
 
 db.sequelize.sync(
-    // {force: true}
+    {force: true}
     ).then(function () {
     app.listen(PORT, function () {
         console.log(`Express is listening on ${PORT}!`);
